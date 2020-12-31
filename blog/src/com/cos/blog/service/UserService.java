cc8506476c1b2aedec332c06660aacb6e038cdec
Lee DaeYeob <tony6303@naver.com>
2020-12-31 17:58:06 +0900

    CharConfig

---
+++ blog/src/com/cos/blog/service/UserService.java
@@ -0,0 +1,28 @@
+package com.cos.blog.service;
+
+import com.cos.blog.domain.user.dto.JoinReqDto;
+import com.cos.blog.domain.user.dto.LoginReqDto;
+import com.cos.blog.domain.user.dto.UpdateReqDto;
+import com.cos.blog.domain.user.User;
+
+public class UserService {
+	public int 회원가입(JoinReqDto dto) {
+
+		return -1; 
+	}
+
+	public User 로그인(LoginReqDto dto) {
+
+		return null;
+	}
+
+	public int 회원수정(UpdateReqDto dto) {
+
+		return -1;
+	}
+
+	public int 아이디중복체크(String username) {
+
+		return -1;
+	}
+}
